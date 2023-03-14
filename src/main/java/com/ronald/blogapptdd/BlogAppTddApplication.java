3d1ff2c93ff07e3d29fc9d2ae5fd742601a20a14
kietna3 <kietna3@vng.com.vn>
2023-03-14 21:35:29 +0700

    init

---
+++ src/main/java/com/ronald/blogapptdd/BlogAppTddApplication.java
@@ -0,0 +1,13 @@
+package com.ronald.blogapptdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ronald/blogapptdd/BlogAppTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronald.blogapptdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
